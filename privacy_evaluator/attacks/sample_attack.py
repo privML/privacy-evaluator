83fc5d9737e80dd6e01c368e43ec2ecbd2aff874
Claas Fandre <64256710+Erdanf@users.noreply.github.com>
2021-05-01 15:08:38 +0200

    first draft of the interface structure

--- privacy_evaluator/attacks/sample_attack.py
+++ privacy_evaluator/attacks/sample_attack.py
@@ -1,1 +1,11 @@
-pass
+from privacy_evaluator.attacks.attack_interface import Attack_Interface
+
+
+class Sample_Attack(Attack_Interface):
+    def __init__(self, model, training_data, test_data):
+        super().__init__(model, training_data, test_data)
+
+    def perform_attack(params):
+        pass
+
+
---
+++ tests/class_test.py
@@ -0,0 +1,4 @@
+from privacy_evaluator.attacks.sample_attack import Sample_Attack
+
+test = Sample_Attack(0, 0, 0)
+test.perform_attack()
