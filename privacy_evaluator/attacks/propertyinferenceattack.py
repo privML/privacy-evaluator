21eda88d18ea8e50e962f8e7db3d72333805da0e
Ina Fendel <i.fendel@gmx.de>
2021-05-11 13:43:56 +0200

    Property Inference Attack interface that inherits from Attack_Interface from team 1 #29

---
+++ privacy_evaluator/attacks/propertyinferenceattack.py
@@ -0,0 +1,27 @@
+import abc
+from privacy_evaluator.attacks.attack_interface import Attack_Interface
+
+
+class PropertyInferenceAttack(Attack_Interface):
+    """
+    Abstract class for property inference classes.
+    """
+
+    def __init__(self, model, property_shadow_training_sets, negation_property_shadow_training_set):
+        """
+        Initialize the Property Inference Attack Class.
+        :param model: the target model to be attacked
+        """
+
+        super().__init__(model, None, None)
+        self.property_shadow_training_sets = property_shadow_training_sets
+        self.negation_shadow_training_sets = negation_property_shadow_training_set
+
+    @abc.abstractmethod
+    def perform_attack(self, params):
+        """
+        Performs the property inference attack on target model.
+        :param params: Example data to run through target model for feature extraction
+        :return: prediction about property of target data set as a result of the property inference attack
+        """
+        raise NotImplementedError
