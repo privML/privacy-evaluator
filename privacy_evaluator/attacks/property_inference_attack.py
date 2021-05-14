f09ce8f35d3465ec849080fb32bc28df7936f7cf
Ina Fendel <i.fendel@gmx.de>
2021-05-14 18:38:35 +0200

    Add datatypes for inputs and outputs #59

--- privacy_evaluator/attacks/property_inference_attack.py
+++ privacy_evaluator/attacks/property_inference_attack.py
@@ -1,4 +1,5 @@
 import abc
+import numpy as np
 from privacy_evaluator.attacks.attack_interface import Attack_Interface
 
 
@@ -16,8 +17,11 @@ class PropertyInferenceAttack(Attack_Interface):
         """
         Initialize the Property Inference Attack Class.
         :param model: the target model to be attacked
+        :type model: :class:`.art.estimators.estimator.BaseEstimator`
         :param property_shadow_training_sets: the shadow training sets that fulfill property
+        :type property_shadow_training_sets: np.ndarray # TODO
         :param negation_property_shadow_training_sets: the shadow training sets that fulfill negation of property
+        :type negation_property_shadow_training_sets: np.ndarray # TODO
         """
 
         super().__init__(model, None, None)
@@ -29,6 +33,8 @@ class PropertyInferenceAttack(Attack_Interface):
         """
         Performs the property inference attack on target model.
         :param params: Example data to run through target model for feature extraction
+        :type params: np.ndarray
         :return: prediction about property of target data set as a result of the property inference attack
+        :rtype: #TODO
         """
         raise NotImplementedError
--- privacy_evaluator/attacks/property_inference_attack_skeleton.py
+++ privacy_evaluator/attacks/property_inference_attack_skeleton.py
@@ -1,18 +1,22 @@
 from privacy_evaluator.attacks.property_inference_attack import PropertyInferenceAttack
+import numpy as np
 
 
 class PropertyInferenceAttackSkeleton(PropertyInferenceAttack):
     def __init__(
-        self,
-        model,
-        property_shadow_training_sets,
-        negation_property_shadow_training_sets,
+            self,
+            model,
+            property_shadow_training_sets,
+            negation_property_shadow_training_sets,
     ):
         """
         Initialize the Property Inference Attack Class.
         :param model: the target model to be attacked
+        :type model: :class:`.art.estimators.estimator.BaseEstimator`
         :param property_shadow_training_sets: the shadow training sets that fulfill property
+        :type property_shadow_training_sets: np.ndarray # TODO
         :param negation_property_shadow_training_sets: the shadow training sets that fulfill negation of property
+        :type negation_property_shadow_training_sets: np.ndarray # TODO
         """
 
         super().__init__(
@@ -26,7 +30,9 @@ class PropertyInferenceAttackSkeleton(PropertyInferenceAttack):
         """
         Train shadow classifiers with each shadow training set (follows property or negation of property).
         :param shadow_training_set: datasets used for shadow_classifiers
+        :type shadow_training_set: np.ndarray # TODO
         :return: shadow classifiers
+        :rtype: "CLASSIFIER_TYPE" (to be found in `.art.utils`)
         """
         raise NotImplementedError
 
@@ -34,7 +40,9 @@ class PropertyInferenceAttackSkeleton(PropertyInferenceAttack):
         """
         Extract the features of a given model.
         :param model: a model from which the features should be extracted
+        :type model: :class:`.art.estimators.estimator.BaseEstimator`
         :return: feature extraction
+        :rtype: np.ndarray
         """
         raise NotImplementedError
 
@@ -42,7 +50,9 @@ class PropertyInferenceAttackSkeleton(PropertyInferenceAttack):
         """
         Create meta training set out of the feature extraction of the shadow classifiers.
         :param feature_extraction_list: list of all feature extractions of all shadow classifiers
+        :type feature_extraction_list: np.ndarray
         :return: Meta-training set
+        :rtype: np.ndarray
         """
         raise NotImplementedError
 
@@ -51,7 +61,9 @@ class PropertyInferenceAttackSkeleton(PropertyInferenceAttack):
         Train meta-classifier with the meta-training set.
         :param meta_training_set: Set of feature representation of each shadow classifier,
         labeled according to whether property or negotiation of property is fulfilled.
+        :type meta_training_set: np.ndarray
         :return: Meta classifier
+        :rtype: "CLASSIFIER_TYPE" (to be found in `.art.utils`) # TODO only binary classifiers - special classifier?
         """
         raise NotImplementedError
 
@@ -59,8 +71,12 @@ class PropertyInferenceAttackSkeleton(PropertyInferenceAttack):
         """
         "Actual" attack: Meta classifier gets feature extraction of target model as input, outputs property prediction.
         :param meta_classifier: A classifier
-        :param feature_extraction_target_model:
+        :type meta_classifier: "CLASSIFIER_TYPE" (to be found in `.art.utils`)
+        # TODO only binary classifiers-special classifier?
+        :param feature_extraction_target_model: extracted features of target model
+        :type feature_extraction_target_model: np.ndarray
         :return: Prediction whether property or negation of property is fulfilled for target data set
+        :rtype: # TODO
         """
         raise NotImplementedError
 
@@ -68,7 +84,9 @@ class PropertyInferenceAttackSkeleton(PropertyInferenceAttack):
         """
         Perform Property Inference attack.
         :param params: Example data to run through target model for feature extraction
+        :type params: np.ndarray
         :return: prediction about property of target data set
+        :rtype: # TODO
         """
         shadow_classifier = self.train_shadow_classifiers(self.shadow_training_set)
         # TODO: create feature extraction for all shadow classifiers
