698e93c51b0b9c07fcfbf9cd15e2205206fab383
yzchyx <yuxuan.chen@fu-berlin.de>
2021-05-21 23:32:49 +0200

    add fullu-connected models for TensorFlow

--- demo/train-cifar10-tf/model.py
+++ demo/train-cifar10-tf/model.py
@@ -1,7 +1,7 @@
 import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
-from tensorflow.keras.layers import Dense, Dropout
+from tensorflow.keras.layers import Dense, Dropout, Flatten
 
 
 class ResNet50(keras.Model):
@@ -40,4 +40,31 @@ class ResNet50(keras.Model):
         out = self.resnet(x)
         out = self.avgpool(out)
         out = self.fc(out)
+        return out
+
+
+class FCNeuralNet(keras.Model):
+    """A simple fully-connected network for multi-classification.
+
+    Args:
+        num_classes: The number of classes involved in the classification.
+        dropout: Drop-out rate in the fully-connected layer.
+    """
+
+    def __init__(self, num_classes: int = 2, dropout: float = 0):
+        super(FCNeuralNet, self).__init__()
+        self.flatten = Flatten()
+        self.fc = keras.Sequential(
+            [
+                Dense(512, activation="relu"),
+                Dropout(dropout),
+                Dense(64, activation="relu"),
+                Dropout(dropout),
+                Dense(num_classes),
+            ]
+        )
+
+    def forward(self, x):
+        x = self.flatten(x)
+        out = self.fc(x)
         return out
