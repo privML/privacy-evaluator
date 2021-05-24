c9769eb873109bded7d568e2cc8bd60042a858f4
florian <florian@kiste.Speedport_W_724V_09011603_06_007>
2021-05-24 15:43:15 +0200

    Black styling. #66

--- tests/attacks/property_inference_test.py
+++ tests/attacks/property_inference_test.py
@@ -1,17 +1,20 @@
-#import privacy_evaluator.attacks.property_inference_attack_skeleton as pia
-from privacy_evaluator.attacks.property_inference_attack_skeleton import (PropertyInferenceAttackSkeleton,)
+# import privacy_evaluator.attacks.property_inference_attack_skeleton as pia
+from privacy_evaluator.attacks.property_inference_attack_skeleton import (
+    PropertyInferenceAttackSkeleton,
+)
 
-#import torch
+# import torch
 from art.estimators.classification import PyTorchClassifier
 import torch.nn as nn
 import torch
 from privacy_evaluator.models.torch import dcti
-#import privacy_evaluator.models.torch.dcti
+
+# import privacy_evaluator.models.torch.dcti
 
 
 model = dcti()
-#model = torch.load("/home/florian/university/SS2021/PrivacyPreservingML/privacy-evaluator/privacy_evaluator/models/torch/torch_fc_class_0_5000_class_1_2000.pth")
-#model.eval()
+# model = torch.load("/home/florian/university/SS2021/PrivacyPreservingML/privacy-evaluator/privacy_evaluator/models/torch/torch_fc_class_0_5000_class_1_2000.pth")
+# model.eval()
 
 criterion = nn.CrossEntropyLoss()
 classifier = PyTorchClassifier(
@@ -21,9 +24,11 @@ classifier = PyTorchClassifier(
     nb_classes=10,
 )
 
-pia_instance = PropertyInferenceAttackSkeleton(0,0,0)
+pia_instance = PropertyInferenceAttackSkeleton(0, 0, 0)
 
-features, labels = pia_instance.create_meta_training_set([classifier, classifier, classifier], [classifier])
+features, labels = pia_instance.create_meta_training_set(
+    [classifier, classifier, classifier], [classifier]
+)
 
 meta_classifier = pia_instance.train_meta_classifier(features, labels)
 
