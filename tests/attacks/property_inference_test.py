89d9ac33d7e7ce2a9237b302a7bc0d750dc6e512
TobiasSchalau <tobias.schalau@fu-berlin.de>
2021-05-25 12:14:06 +0200

    First implementation of test file for PIA with pytest. #69

--- tests/attacks/property_inference_test.py
+++ tests/attacks/property_inference_test.py
@@ -1,42 +1,18 @@
-# import privacy_evaluator.attacks.property_inference_attack_skeleton as pia
+import pytest
+
 from privacy_evaluator.attacks.property_inference_attack_skeleton import (
     PropertyInferenceAttackSkeleton,
 )
-
-# import torch
-from art.estimators.classification import PyTorchClassifier
-import torch.nn as nn
-import torch
-from privacy_evaluator.models.torch import dcti
-
-# import privacy_evaluator.models.torch.dcti
+from privacy_evaluator.models.torch.dcti import load_dcti
+from privacy_evaluator.datasets.cifar10 import CIFAR10
+from privacy_evaluator.classifiers.classifier import Classifier
 
 
-model = dcti()
-# model = torch.load("/home/florian/university/SS2021/PrivacyPreservingML/privacy-evaluator/privacy_evaluator/models/torch/torch_fc_class_0_5000_class_1_2000.pth")
-# model.eval()
-
-criterion = nn.CrossEntropyLoss()
-classifier = PyTorchClassifier(
-    model=model,
-    loss=criterion,
-    input_shape=(1, 28, 28),
-    nb_classes=10,
-)
-
-pia_instance = PropertyInferenceAttackSkeleton(0, 0, 0)
-
-features, labels = pia_instance.create_meta_training_set(
-    [classifier, classifier, classifier], [classifier]
-)
-
-meta_classifier = pia_instance.train_meta_classifier(features, labels)
-
-print(meta_classifier.predict(features[0].reshape(1, -1)))
-
-"""
-import os
-os.chdir('/home/florian/university/SS2021/PrivacyPreservingML/privacy-evaluator')
-
-model1 = torch.load("/home/florian/university/SS2021/PrivacyPreservingML/privacy-evaluator/privacy_evaluator/models/torch/torch_fc_class_0_5000_class_1_2000.pth")
-"""
+def test_membership_inference_attack():
+    x_train, y_train, x_test, y_test = CIFAR10.numpy()
+    target_model = Classifier(
+        load_dcti(), nb_classes=CIFAR10.N_CLASSES, input_shape=CIFAR10.INPUT_SHAPE
+    )
+    attack = PropertyInferenceAttackSkeleton(target_model) #TODO inputs correct?
+    with pytest.raises(NotImplementedError):
+        attack.perform_attack() #TODO no parameter?
