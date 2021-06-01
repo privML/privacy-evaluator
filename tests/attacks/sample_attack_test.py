51bd5ea688775a6cd04a6714c4dc6afcc4ef63b9
Jakob Heinrich <reg@jakobheinrich.de>
2021-06-01 12:43:10 +0200

    ... it's not that i fogot some more tests ... I'm just doing this to get a better individual grade ...

--- tests/attacks/sample_attack_test.py
+++ tests/attacks/sample_attack_test.py
@@ -5,11 +5,16 @@ from privacy_evaluator.models.torch.dcti.dcti import load_dcti
 from privacy_evaluator.datasets.cifar10 import CIFAR10
 from privacy_evaluator.classifiers.classifier import Classifier
 
+import torch.nn as nn
+
 
 def test_sample_attack():
     x_train, y_train, x_test, y_test = CIFAR10.numpy()
     target_model = Classifier(
-        load_dcti(), nb_classes=CIFAR10.N_CLASSES, input_shape=CIFAR10.INPUT_SHAPE
+        load_dcti(),
+        nb_classes=CIFAR10.N_CLASSES,
+        input_shape=CIFAR10.INPUT_SHAPE,
+        loss=nn.CrossEntropyLoss(reduction="none"),
     )
 
     attack = SampleAttack(target_model, x_train, y_train, x_test, y_test)
