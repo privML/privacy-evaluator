75d24b96f005a9809ec7b06c0bce6eef0d449fcf
Jakob Heinrich <reg@jakobheinrich.de>
2021-06-01 12:12:10 +0200

    changed test to work with new Classifier Argument (lossfn)

--- tests/classifiers/classifier_test.py
+++ tests/classifiers/classifier_test.py
@@ -1,11 +1,15 @@
 from privacy_evaluator.models.torch.dcti.dcti import load_dcti
 from privacy_evaluator.datasets.cifar10 import CIFAR10
 from privacy_evaluator.classifiers.classifier import Classifier
+import torch.nn as nn
 
 
 def test_classifier():
     x_train, _, _, _ = CIFAR10.numpy()
     classifier = Classifier(
-        load_dcti(), nb_classes=CIFAR10.N_CLASSES, input_shape=CIFAR10.INPUT_SHAPE
+        load_dcti(),
+        nb_classes=CIFAR10.N_CLASSES,
+        input_shape=CIFAR10.INPUT_SHAPE,
+        loss=nn.CrossEntropyLoss(reduction="none"),
     )
     assert classifier.predict(x_train).shape == (x_train.shape[0], CIFAR10.N_CLASSES)
--- tests/metrics/privacy_risk_score_test.py
+++ tests/metrics/privacy_risk_score_test.py
@@ -8,14 +8,17 @@ import torch.nn as nn
 from privacy_evaluator.models.torch.dcti.dcti import load_dcti
 from privacy_evaluator.datasets.cifar10 import CIFAR10
 
-from privacy_evaluator.metrics import privacy_risk_score
+from privacy_evaluator.metrics import compute_privacy_risk_score
 from privacy_evaluator.classifiers.classifier import Classifier
 
 
 def test_privacy_risk_score():
     x_train, y_train, x_test, y_test = CIFAR10.numpy()
     classifier = Classifier(
-        load_dcti(), nb_classes=CIFAR10.N_CLASSES, input_shape=CIFAR10.INPUT_SHAPE
+        load_dcti(),
+        nb_classes=CIFAR10.N_CLASSES,
+        input_shape=CIFAR10.INPUT_SHAPE,
+        loss=nn.CrossEntropyLoss(reduction="none"),
     )
-    score = privacy_risk_score(classifier, x_train, y_train, x_test, y_test)
+    score = compute_privacy_risk_score(classifier, x_train, y_train, x_test, y_test)
     assert bool(score)
