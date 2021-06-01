d364f1a2cc6dd261945b3c3a5da6860375ee50c9
Jakob Heinrich <reg@jakobheinrich.de>
2021-06-01 12:38:46 +0200

    changed some more tests which I forgot before

--- tests/attacks/membership_inference/black_box_rule_based_test.py
+++ tests/attacks/membership_inference/black_box_rule_based_test.py
@@ -5,11 +5,16 @@ from privacy_evaluator.models.torch.dcti.dcti import load_dcti
 from privacy_evaluator.datasets.cifar10 import CIFAR10
 from privacy_evaluator.classifiers.classifier import Classifier
 
+import torch.nn as nn
+
 
 def test_membership_inference_black_box_rule_based_attack():
     x_train, y_train, x_test, y_test = CIFAR10.numpy()
     target_model = Classifier(
-        load_dcti(), nb_classes=CIFAR10.N_CLASSES, input_shape=CIFAR10.INPUT_SHAPE
+        load_dcti(),
+        nb_classes=CIFAR10.N_CLASSES,
+        input_shape=CIFAR10.INPUT_SHAPE,
+        loss=nn.CrossEntropyLoss(reduction="none"),
     )
     attack = MembershipInferenceBlackBoxRuleBasedAttack(
         target_model, x_train[:100], y_train[:100], x_test[:100], y_test[:100]
