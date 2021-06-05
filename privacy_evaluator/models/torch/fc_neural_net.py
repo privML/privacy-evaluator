f5e55adc3c8352fa5e569a4370712698880712a7
yzchyx <yuxuan.chen@fu-berlin.de>
2021-06-05 13:48:30 +0200

    brute-force define two FCNets to deal with different input shapes

--- privacy_evaluator/models/torch/fc_neural_net.py
+++ privacy_evaluator/models/torch/fc_neural_net.py
@@ -2,13 +2,27 @@ from torch import nn
 from typing import Tuple
 import math
 
-class FCNeuralNet(nn.Module):
+
+def FCNeuralNet(
+    num_classes: int = 2,
+    dropout: float = 0,
+    input_shape: Tuple[int, ...] = (32, 32, 3)
+) -> Optional[torch.Module]:
     """A simple fully-connected network for multi-classification.
 
     Args:
         num_classes: The number of classes involved in the classification.
         dropout: Drop-out rate in the fully-connected layer.
+        input_shape: the shape of one data point,
+            for CIFAR: [32, 32, 3], for MNIST [28, 28]
     """
+    if input_shape == (32, 32, 3):
+        return FCNeuralNet32X32X3(num_classes, dropout)
+    elif input_shape in [(28, 28), (28, 28,), (28, 28, 1)]:
+        return FCNeuralNet28X28X1(num_classes, dropout)
+
+
+class FCNeuralNet32X32X3(torch.Module):
     def __init__(
             self,
             num_classes: int = 2,
@@ -16,8 +30,34 @@ class FCNeuralNet(nn.Module):
     ):
         super(FCNeuralNet, self).__init__()
         self.flatten = nn.Flatten()
+        self.input_layer = nn.Linear()
         self.fc = nn.Sequential(
             nn.Linear(32 * 32 * 3, 512),  # TODO: retrieve later
+            nn.ReLU(),
+            nn.Dropout(dropout),
+            nn.Linear(512, 64),
+            nn.ReLU(),
+            nn.Dropout(dropout),
+            nn.Linear(64, num_classes),
+        )
+
+    def forward(self, x):
+        out = self.flatten(x)
+        out = self.fc(out)
+        return out
+
+
+class FCNeuralNet28X28X1(torch.Module):
+    def __init__(
+            self,
+            num_classes: int = 2,
+            dropout: float = 0,
+    ):
+        super(FCNeuralNet, self).__init__()
+        self.flatten = nn.Flatten()
+        self.input_layer = nn.Linear()
+        self.fc = nn.Sequential(
+            nn.Linear(28 * 28 * 1, 512),  # TODO: retrieve later
             nn.ReLU(),
             nn.Dropout(dropout),
             nn.Linear(512, 64),
