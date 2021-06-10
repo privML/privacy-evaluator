3474dff862b1a94c9af9687c13650a14d3920896
Claas Fandre <64256710+Erdnaf@users.noreply.github.com>
2021-06-10 17:19:02 +0200

    Feat (user-output): Improve User Output
    See: !122.
    Closes: #118.

--- privacy_evaluator/output/user_output.py
+++ privacy_evaluator/output/user_output.py
@@ -13,7 +13,6 @@ class UserOutput:
         self,
         attack_data_y: np.ndarray,
         privacy_risk: np.ndarray,
-        all_labels: np.ndarray,
     ):
         """
         Initilaizes the Class with values
@@ -23,13 +22,13 @@ class UserOutput:
         """
         self.attack_data_y = attack_data_y
         self.privacy_risk = privacy_risk
-        self.all_labels = all_labels
 
     def histogram_top_k(
-        self, k: int = 10, show_diagram: bool = True
+        self, all_labels: np.ndarray, k: int = 10, show_diagram: bool = True
     ) -> Tuple[np.ndarray, np.ndarray]:
         """
         Draw histogram of class distribution of the k points with highest privacy risk score
+        :param all_labels: all the labels of the input data
         :param k: the number of points to consider, default 10
         :param show_diagram: determines if the diagram should be shown, default: True
         :return: All lables of the data with the number of points that are in the top k
@@ -46,14 +45,59 @@ class UserOutput:
         labels = df.index.to_numpy()
         count = df["count"].to_numpy()
         all_counts = np.array([])
-        for label in self.all_labels:
+        for label in all_labels:
             index = np.where(labels == label)
             if len(index[0]) == 0:
                 all_counts = np.append(all_counts, 0)
             else:
                 all_counts = np.append(all_counts, count[index[0]])
         if show_diagram:
-            plt.bar(self.all_labels, all_counts)
-            plt.title("histogram")
+            plt.bar(all_labels, all_counts)
+            plt.title("Histogram for top {} points per label".format(k))
+            plt.yticks(np.arange(0, np.int_(max(all_counts)) + 1, 1))
             plt.show()
-        return self.all_labels, all_counts
+        return all_labels, all_counts
+
+    def histogram_top_k_relative(
+        self,
+        all_labels: np.ndarray,
+        k: int = 10,
+        show_diagram: bool = True,
+    ) -> Tuple[np.ndarray, np.ndarray]:
+        """
+        Draw histogram of class distribution of the k points with highest privacy risk score, relative to the size of the classes
+        :param all_labels: all the labels of the input data
+        :param k: the number of points to consider, default 10
+        :param show_diagram: determines if the diagram should be shown, default: True
+        :return: All lables of the data with the number of points that are in the top k, relative to the size of the classes
+        """
+        sorting = np.argsort(self.privacy_risk)
+        sorting = np.flip(sorting)
+        sorted_attack_data_labels = self.attack_data_y[sorting][:k]
+        df = pd.DataFrame(
+            {
+                "labels": sorted_attack_data_labels,
+            }
+        )
+        df = df.groupby("labels")["labels"].agg(["count"])
+        labels = df.index.to_numpy()
+        count = df["count"].to_numpy()
+        all_counts = np.array([])
+        label_counts = np.array([])
+        for label in all_labels:
+            label_count = (self.attack_data_y == label).sum()
+            label_counts = np.append(label_counts, label_count)
+            index = np.where(labels == label)
+            if len(index[0]) == 0:
+                all_counts = np.append(all_counts, 0)
+            else:
+                all_counts = np.append(all_counts, count[index[0]])
+        relative_values = np.divide(all_counts, label_counts)
+        relative_values = np.nan_to_num(
+            relative_values, copy=True, nan=0.0, posinf=None, neginf=None
+        )
+        if show_diagram:
+            plt.bar(all_labels, relative_values)
+            plt.title("Histogram for top {} points relative per class".format(k))
+            plt.show()
+        return all_labels, relative_values
--- tests/output/output_test.py
+++ tests/output/output_test.py
@@ -9,9 +9,26 @@ def test_output_function():
         ["blue", "orange", "red", "orange", "red", "red", "blue", "red", "orange"]
     )
     priv_risk = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9])
-    user_output = UserOutput(
-        data_y, priv_risk, ["green", "blue", "red", "orange", "white"]
+    user_output = UserOutput(data_y, priv_risk)
+    labels, count = user_output.histogram_top_k(
+        np.array(["green", "blue", "red", "orange", "white"]),
+        4,
+        show_diagram=False,
     )
-    labels, count = user_output.histogram_top_k(4, show_diagram=False)
     assert (labels == np.array(["green", "blue", "red", "orange", "white"])).all()
     assert (count == np.array([0, 1, 2, 1, 0])).all()
+
+
+def test_output_function_relative():
+    data_y = np.array(
+        ["blue", "orange", "red", "orange", "red", "red", "blue", "red", "orange"]
+    )
+    priv_risk = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9])
+    user_output = UserOutput(data_y, priv_risk)
+    labels, count = user_output.histogram_top_k_relative(
+        np.array(["green", "blue", "red", "orange", "white"]),
+        4,
+        show_diagram=False,
+    )
+    assert (labels == np.array(["green", "blue", "red", "orange", "white"])).all()
+    np.testing.assert_array_almost_equal(count, np.array([0, 0.5, 0.5, 0.333333, 0]))
