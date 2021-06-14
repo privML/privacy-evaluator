5931095a4f7b86183de1be9a2b5e02667a77032e
yzchyx <yuxuan.chen@fu-berlin.de>
2021-06-14 23:16:38 +0200

    fix the bug that deepcopy does not work for TF

--- privacy_evaluator/utils/model_utils.py
+++ privacy_evaluator/utils/model_utils.py
@@ -4,9 +4,7 @@ from privacy_evaluator.models.tf.fc_neural_net import FCNeuralNet
 from privacy_evaluator.utils.data_utils import dataset_downloader, new_dataset_from_size_dict
 from privacy_evaluator.utils.trainer import trainer, tester
 
-import tensorflow as tf
 from tensorflow import keras
-import torch
 from torch import nn
 from art.estimators.estimator import BaseEstimator
 import numpy as np
@@ -14,7 +12,7 @@ from copy import deepcopy
 from typing import Union
 
 
-def copy_model_and_reset(
+def copy_and_reset_model(
     target_model: Union[nn.Module, keras.Model, BaseEstimator]
 ) -> Union[nn.Module, keras.Model]:
     """Copy the architechture of a given tf or torch model and reset the weights.
@@ -23,17 +21,17 @@ def copy_model_and_reset(
     :return: the clone of it with newly initialized weights on the same platform
     """
     if isinstance(target_model, BaseEstimator):
-        return copy_model_and_reset(target_model.model)
+        return copy_and_reset_model(target_model.model)
     
     elif isinstance(target_model, nn.Module):
-        return _copy_torch_model_and_reset(target_model)
+        return _copy_and_reset_torch_model(target_model)
     elif isinstance(target_model, keras.Model):
-        return _copy_tf_model_and_reset(target_model)
+        return _copy_and_reset_tf_model(target_model)
     else:
         raise TypeError("Unsupported model type!")
 
 
-def _copy_torch_model_and_reset(target_model: nn.Module) -> nn.Module:
+def _copy_and_reset_torch_model(target_model: nn.Module) -> nn.Module:
     model = deepcopy(target_model)
     for layers in model.children():
         if hasattr(layers, "__iter__"):
@@ -45,8 +43,9 @@ def _copy_torch_model_and_reset(target_model: nn.Module) -> nn.Module:
     return model
 
 
-def _copy_tf_model_and_reset(target_model: keras.Model) -> keras.Model:
-    model = deepcopy(target_model)
+def _copy_and_reset_tf_model(target_model: keras.Model) -> keras.Model:
+    target_model.save('./target_model')
+    model = keras.models.load_model('./target_model')
     for layer in model.trainable_variables:
         # the default kernel initializer used by Keras is normal distribution
         layer_shape = layer.numpy().shape
@@ -60,9 +59,12 @@ if __name__ == "__main__":
     size_dict = {0: 1000, 1: 1000}
     train_set = new_dataset_from_size_dict(train_set, size_dict)
     
-    target_model = ConvNetCIFAR10()
+    target_model = FCNeuralNet()
     trainer(train_set, size_dict, target_model)
     print(tester(train_set, size_dict, target_model))
 
-    copied_model = copy_model_and_reset(target_model)
+    copied_model = copy_and_reset_model(target_model)
+    print(tester(train_set, size_dict, copied_model))
+    print(tester(train_set, size_dict, target_model))
+    trainer(train_set, size_dict, copied_model)
     print(tester(train_set, size_dict, copied_model))
