88f777fa974dc5d7f15c5cf4d278b197f9ef2c78
florian <florian.mies@fu-berlin.de>
2021-06-15 10:48:10 +0200

    Remove created target_model folder after saving and reloading it. #132

--- privacy_evaluator/utils/model_utils.py
+++ privacy_evaluator/utils/model_utils.py
@@ -1,19 +1,10 @@
-from tensorflow.keras import layers
-from privacy_evaluator.models.tf.cnn import ConvNetMNIST, ConvNetCIFAR10
-from privacy_evaluator.models.tf.fc_neural_net import FCNeuralNet
-from privacy_evaluator.utils.data_utils import (
-    dataset_downloader,
-    new_dataset_from_size_dict,
-)
-from privacy_evaluator.utils.trainer import trainer, tester
-
 from tensorflow import keras
 from torch import nn
 from art.estimators.estimator import BaseEstimator
 import numpy as np
 from copy import deepcopy
 from typing import Union
-
+import shutil
 
 def copy_and_reset_model(
     model: Union[keras.Model, nn.Module, BaseEstimator]
@@ -31,7 +22,7 @@ def copy_and_reset_model(
 def _copy_and_reset_torch_model(target_model):
     model = deepcopy(target_model)
     for layers in model.children():
-        if hasattr(layers, "iter"):
+        if hasattr(layers, "__iter__"):
             for layer in layers:
                 if hasattr(layer, "reset_parameters"):
                     layer.reset_parameters()
@@ -43,24 +34,12 @@ def _copy_and_reset_torch_model(target_model):
 def _copy_and_reset_tf_model(target_model: keras.Model) -> keras.Model:
     target_model.save("./target_model")
     model = keras.models.load_model("./target_model")
+    shutil.rmtree("./target_model")
     for layer in model.trainable_variables:
         # the default kernel initializer used by Keras is normal distribution
         layer_shape = layer.numpy().shape
-        layer.assign(np.random.normal(0, 1, layer_shape))
+        #layer.assign(np.random.normal(0, 1, layer_shape))
+        layer.assign(np.zeros(layer.shape))
     return model
 
 
-if __name__ == "__main__":
-    train_set, _ = dataset_downloader("CIFAR10")
-    size_dict = {0: 1000, 1: 1000}
-    train_set = new_dataset_from_size_dict(train_set, size_dict)
-
-    target_model = FCNeuralNet()
-    trainer(train_set, size_dict, target_model)
-    print(tester(train_set, size_dict, target_model))
-
-    copied_model = copy_and_reset_model(target_model)
-    print(tester(train_set, size_dict, copied_model))
-    print(tester(train_set, size_dict, target_model))
-    trainer(train_set, size_dict, copied_model)
-    print(tester(train_set, size_dict, copied_model))
