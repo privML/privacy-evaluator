694fb7183711932ff0ab3880a8fded5946e9175d
Franziska Boenisch <franziska.boenisch.fb@gmail.com>
2021-06-21 06:12:15 +0200

    Update __init__.py

--- privacy_evaluator/attacks/__init__.py
+++ privacy_evaluator/attacks/__init__.py
@@ -1,8 +1,6 @@
 """
 Module providing attacks.
 """
-
-
 from privacy_evaluator.attacks import membership_inference
 from privacy_evaluator.attacks.attack import Attack
 from privacy_evaluator.attacks.sample_attack import SampleAttack
