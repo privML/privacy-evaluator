d4f4f6082ecd74bc57425b2da9d682ca0a0987e9
Claas Fandre <64256710+Erdnaf@users.noreply.github.com>
2021-06-22 09:39:32 +0200

    user output in slicing (#167)
    * First integration of user output into slicing
    
    * Fix JSON serialization error. Added accuracy score to the result.
    
    * Apply black
    
    * Add missing return statement in to_json method
    
    Co-authored-by: juri <hello@jtorhoff.com>

---
+++ privacy_evaluator/output/user_output_inference_attack_analysis.py
@@ -0,0 +1,58 @@
+from textwrap import indent
+from typing import Tuple
+
+import numpy as np
+
+from ..attacks.membership_inference.data_structures.slicing import Slice
+from .user_output import UserOutput
+
+
+class UserOutputInferenceAttackAnalysis(UserOutput):
+    """Result of the membership inference attack analysis for a single slice."""
+
+    def __init__(
+        self,
+        slice: Slice,
+        advantage: float,
+        accuracy: float,
+    ):
+        self.slice = slice
+        self.advantage = advantage
+        self.accuracy = accuracy
+
+    def to_json(self, include_indices=False) -> str:
+        """Serialize the output to JSON.
+
+        :param include_indices: If True, slice indices will be included in the output.
+        """
+        if include_indices:
+            slice = Slice(
+                indices=self.slice.indices.tolist(),
+                desc=self.slice.desc,
+            )
+        else:
+            slice = Slice(
+                indices=[],
+                desc=self.slice.desc,
+            )
+
+        output = UserOutputInferenceAttackAnalysis(
+            slice=slice.__dict__,
+            advantage=self.advantage,
+            accuracy=self.accuracy,
+        )
+
+        return UserOutput._to_json(
+            output,
+        )
+
+    def __str__(self) -> str:
+        return "\n".join(
+            (
+                "UserOutputInferenceAttackAnalysis(",
+                indent(str(self.slice), "  "),
+                f"  advantage: {self.advantage:.3f}",
+                f"  accuracy: {self.accuracy:.3f}",
+                ")",
+            )
+        )
