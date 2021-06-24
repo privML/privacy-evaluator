56ad4c4cf4682d43f0d9bd06491bb27b89135b8e
Ina Fendel <i.fendel@gmx.de>
2021-06-24 12:54:00 +0200

    Change hard-coded values to global variables to have uniform design #136

--- tests/attacks/property_inference_test.py
+++ tests/attacks/property_inference_test.py
@@ -6,17 +6,40 @@ from privacy_evaluator.utils.data_utils import (
 )
 from privacy_evaluator.utils.trainer import trainer
 from privacy_evaluator.models.torch.cnn import ConvNet
-from typing import Dict
+from typing import Dict, List
 from torch import nn
 
-
+#ratio for target model
 NUM_ELEMENTS_PER_CLASSES = {0: 1000, 1: 1000}
+#dataset for the attack
 DATASET = "MNIST"
+#number of channels for CNN
+NUM_CHANNELS = (1, 16, 32, 64)
+#number of epochs for trainer
+NUM_EPOCHS = 2
+# count of shadow training sets, must be even
+AMOUNT_SETS = 2
+# ratio and size for unbalanced data sets
+SIZE_SET = 100
+# ratios for different properties in sub-attacks
+RATIOS_FOR_ATTACK = [0.9, 0.3]
+# classes the attack should be performed on
+CLASSES = [4,5]
+#0: no information; 1: backbone (most important) information; 2: utterly detailed
+VERBOSE = 1
+
 
 
 def test_property_inference_attack(
     num_elements_per_classes: Dict[int, int] = NUM_ELEMENTS_PER_CLASSES,
     dataset: str = DATASET,
+    num_channels: int = NUM_CHANNELS,
+    num_epochs: int = NUM_EPOCHS,
+    amount_sets: int = AMOUNT_SETS,
+    size_set: int = SIZE_SET,
+    ratios_for_attack: List[float] = RATIOS_FOR_ATTACK,
+    classes: List[int] = CLASSES,
+    verbose: int = VERBOSE
 ):
     train_dataset, test_dataset = dataset_downloader(dataset)
     input_shape = test_dataset[0][0].shape
@@ -26,27 +49,22 @@ def test_property_inference_attack(
     train_set = new_dataset_from_size_dict(train_dataset, num_elements_per_classes)
     # num_channels and input_shape are optional in cnn.py
     model = ConvNet(
-        num_classes, input_shape, num_channels=(input_shape[-1], 16, 32, 64)
+        num_classes, input_shape, num_channels= num_channels
     )
 
     print("Start training target model ...\n")
-    trainer(train_set, num_elements_per_classes, model, num_epochs=2)
+    trainer(train_set, num_elements_per_classes, model, num_epochs= num_epochs)
 
     # change pytorch classifier to art classifier
     target_model = Classifier._to_art_classifier(
         model, "sparse_categorical_crossentropy", num_classes, input_shape
     )
     print("Start attack ...")
-    # test parameters for PIA:
-    amount_sets = 2
-    size_set = 100
-    ratios_for_attack = [0.9, 0.3]
-    classes = [4, 5]
 
     attack = PropertyInferenceAttack(
         target_model,
         train_dataset,
-        verbose=1,
+        verbose= verbose,
         size_set=size_set,
         ratios_for_attack=ratios_for_attack,
         classes=classes,
@@ -59,7 +77,6 @@ def test_property_inference_attack(
         attack.amount_sets >= 2 and attack.amount_sets % 2 == 0
     ), "Number of shadow classifiers must be even and greater than 1."
     output = attack.attack()
-
     # we expect the ratios to be ordered
     ratios_for_attack.sort()
 
