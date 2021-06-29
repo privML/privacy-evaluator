251344633079a7ca04470b50d905b2bc7fb99f4e
Friedrich-Mueller <fmjobline@gmail.com>
2021-06-29 12:22:12 +0200

    initial injection of interpretations into the library #154

--- privacy_evaluator/attacks/membership_inference/membership_inference_analysis.py
+++ privacy_evaluator/attacks/membership_inference/membership_inference_analysis.py
@@ -13,7 +13,43 @@ from sklearn import metrics
 
 
 class MembershipInferenceAttackAnalysis:
-    """Represents the membership inference attack analysis class."""
+    """Represents the membership inference attack analysis class.
+
+    Interpretation of Outcome:
+
+    Advantage Score:
+    The attacker advantageis a score that relies on comparing the model output on member and non-member data points.
+    The model outputs are probability values over all classes, and they are often different on member and non-member
+    data points. Usually, the model is more confident on member data points, because it has seen them during training.
+    When trying to find a threshold value to tell apart member and non-member samples by their different model outputs,
+    the attacker has interest in finding the best ratio between false positives “fpr” (non-members that are classified
+    as members) and true positives “tpr” (members that are correctly identifies as members).
+    This best ratio is calculated as the max(tpr-fpr) over all threshold values and represents the attacker advantage.
+
+    Slicing: Incorrectly classified:
+    It is normal that the attacker is more successful to deduce membership on incorrectly classified samples than on
+    correctly classified ones. This results from the fact, that model predictions are often better on training than on
+    test data points, whereby your attack model might learn to predict incorrectly classified samples as non-members.
+    If your model overfits the training data, this assumption might hold true often enough to make the attack seem more
+    successful on this slice. If you wish to reduce that, pay attention to reducing your model’s overfitting.
+
+    Slicing: Specific classes:
+    Specific classes can be differently vulnerable. It may seem that the membership inference attack is more successful
+    on some classes than on the other classes. Research has shown that the class distribution (and also the distribution
+    of data points within one class) are factors that influence the vulnerability of a class for membership inference
+    attacks [1]. Also, small classes (belonging to minority groups) can be more prone to membership inference attacks[2].
+    One reason for this could be, that there is less data for that class, and therefore, the model overfits within this
+    class. It might make sense to look into the vulnerable classes of your model again, and maybe add more data to them,
+    use private synthetic data, or introduce privacy methods like Differential Privacy [2]. Attention, the use of
+    Differential Privacy could have a negative influence on the performance of your model for the minority classes.
+
+    References:
+    [1] Stacey Truex, Ling Liu, Mehmet Emre Gursoy, Lei Yu, and Wenqi Wei. 2019.Demystifying Membership Inference
+    Attacks in Machine Learning as a Service.IEEE Transactions on Services Computing(2019)
+    [2] Suriyakumar, Vinith M., Nicolas Papernot, Anna Goldenberg, and Marzyeh Ghassemi. "Chasing Your Long Tails:
+    Differentially Private Prediction in Health Care Settings." In Proceedings of the 2021 ACM Conference on Fairness,
+    Accountability, and Transparency, pp. 723-734. 2021.
+    """
 
     def __init__(
         self, attack_type: Type[MembershipInferenceAttack], input_data: AttackInputData
