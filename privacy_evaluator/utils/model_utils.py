9def13c309896c51f157011365196b6e0cddef45
yzchyx <yuxuan.chen@fu-berlin.de>
2021-06-30 16:37:42 +0200

    correct some comment for `copy_and_reset_model`

--- privacy_evaluator/utils/model_utils.py
+++ privacy_evaluator/utils/model_utils.py
@@ -2,6 +2,7 @@ from tensorflow import keras
 from torch import nn
 from art.estimators.estimator import BaseEstimator
 import numpy as np
+import tensorflow as tf
 from copy import deepcopy
 from typing import Union
 import shutil
@@ -14,11 +15,13 @@ def copy_and_reset_model(
     Get a clone of a model (i.e. with the same architecture) and reset the weights \
         of this copy.
 
-    :param model: A TF or Torch model. Alternatively, an art-classifier encapsulating \
-        a TF or Torch model.
-    :return: The re-initialized copy of the given model. If `model` is a TF model \
-        (or art-TF) then we return a TF model; the same applied in Torch case.
+    :param model: A tf(keras) or torch model. Alternatively, an art-classifier encapsulating \
+        a keras or torch model.
+    :return: The re-initialized copy of the given model. If `model` is a keras model \
+        (or art-keras) then we return a keras model; If `model` is a torch model (or art-torch) \
+        then we return a torch model.
     """
+    assert isinstance(model, tf.Module)
     if isinstance(model, nn.Module):
         return _copy_and_reset_torch_model(model)
     if isinstance(model, keras.Model):
