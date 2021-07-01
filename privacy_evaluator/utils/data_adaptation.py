a48686266ca417adc37e651b3690d7092532bc7f
Ina Fendel <i.fendel@gmx.de>
2021-07-01 16:53:17 +0200

    Add brightness adaption option for images #174

--- privacy_evaluator/utils/data_adaptation.py
+++ privacy_evaluator/utils/data_adaptation.py
@@ -5,6 +5,7 @@ from copy import deepcopy
 
 
 BOX_LEN = 3  # default side length of the box we use for mask-adaptation
+BRIGHTNESS= 50 # default value to brighten pictures
 
 
 def images_adaptation(
@@ -14,7 +15,7 @@ def images_adaptation(
     Apply a specific adaptation on each image in `images`.
 
     :params images: The original images of shape [N, H, W, D].
-    :params adaptation: The type of adaptation, so far "mask" is supported.
+    :params adaptation: The type of adaptation, so far "mask", is supported.
     :params **kwargs: Optional parameters for the specified adaptation.
     :return: The adapted images.
 
@@ -23,7 +24,7 @@ def images_adaptation(
             of side length 5.
     """
     supported_adaptations = [
-        "mask",
+        "mask", "brightness"
     ]
     assert len(images.shape) == 4
     assert adaptation in supported_adaptations
@@ -34,7 +35,12 @@ def images_adaptation(
             if "box_len" not in kwargs
             else _mask_images(images, kwargs["box_len"])
         )
-
+    elif adaptation == "brightness":
+        return (
+            _brighten_images(images)
+            if "brightness" not in kwargs
+            else _brighten_images(images, kwargs["brightness"])
+        )
 
 def _mask_images(images: np.ndarray, box_len: int = BOX_LEN) -> np.ndarray:
     """
@@ -65,3 +71,34 @@ def _mask_image(image: np.ndarray, box_len: int = BOX_LEN):
     start_y = np.random.randint(0, height - box_len + 1)
 
     image[start_x : start_x + box_len, start_y : start_y + box_len] = 255
+
+
+def _brighten_images(images: np.ndarray, brightness: int = BRIGHTNESS) -> np.ndarray:
+    """
+    Adjust the brightness of all input images
+
+    :params images: The original images of shape [H, W, D].
+    :params brightness: The amount the brighness should be raised or lowered
+    :return: Images with adjusted brightness
+    """
+    brighten_images = deepcopy(images)
+    for image in brighten_images:
+        _brighten_image(image, brightness)
+    return brighten_images
+
+def _brighten_image(image: np.ndarray, brightness: int):
+    """
+    Adjust the brightness of one image 
+
+    :params image: The original image of shape [H, W, D].
+    :params brightness: The amount the brightness should be raised or lowered
+    """
+    height, width, _ = image.shape
+    for x in range(height):
+        for y in range(width):
+            if image[x,y] < (0 - brightness):
+                image[x,y] = 0
+            elif image[x,y] > (255 - brightness):
+                 image[x,y] = 255
+            else:
+                image[x,y] = image[x,y]+brightness
