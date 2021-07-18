7cfa29b25fc53ca55e9223898266234983e00bbc
travela <florian.mies@fu-berlin.de>
2021-07-18 13:49:49 +0200

    Update user_output_property_inference_attack.py
    Remove doc string for __init__ method.

--- privacy_evaluator/output/user_output_property_inference_attack.py
+++ privacy_evaluator/output/user_output_property_inference_attack.py
@@ -7,8 +7,5 @@ class UserOutputPropertyInferenceAttack(UserOutput):
     """User Output for Inference Attacks Class"""
 
     def __init__(self, max_message: str, output: Dict[str, float]):
-        """
-        Initilaizes the Class with values
-        """
         self.max_message = max_message
         self.output = output
